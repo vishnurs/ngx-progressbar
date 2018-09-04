59a74761748be9737bc3d985089345da815567dd
wishnu <vsureshkumar@netsurion.com>
2018-09-04 11:02:51 +0530

    add silent apis

--- lib/http/src/ng-progress-http.module.ts
+++ lib/http/src/ng-progress-http.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgProgressInterceptor } from './ng-progress.interceptor';
+
+import { CONFIG } from './ng-progress-http.token';
 
 @NgModule({
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: NgProgressInterceptor, multi: true }
   ],
 })
-export class NgProgressHttpModule {}
+const defaultConfig = {
+  silentApis: []
+}
+export class NgProgressHttpModule {
+  static forRoot(config?): ModuleWithProviders {
+    config = {...defaultConfig, ...config};
+    return {
+      ngModule: NgProgressHttpModule,
+      providers: [
+        { provide: CONFIG, useValue: config },
+        { provide: HTTP_INTERCEPTORS, useClass: NgProgressInterceptor, multi: true }
+      ]
+    };
+  }
+}
--- lib/http/src/ng-progress.interceptor.ts
+++ lib/http/src/ng-progress.interceptor.ts
@@ -1,19 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional, Inject } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { finalize } from 'rxjs/operators/finalize';
 import { NgProgress } from '@ngx-progressbar/core';
+import { CONFIG } from './ng-progress-http.token';
+
 
 @Injectable()
 export class NgProgressInterceptor implements HttpInterceptor {
 
   private _inProgressCount = 0;
 
-  constructor(private _ngProgress: NgProgress) {
+  constructor(private _ngProgress: NgProgress, @Optional() @Inject(CONFIG) private _config?) {
   }
 
   // Ignoring specific requests will be supported after this https://github.com/angular/angular/issues/18155
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.checkUrl(req)) {
+      return next.handle(req);
+    }
     this._inProgressCount++;
     if (!this._ngProgress.ref('root').isStarted) {
       this._ngProgress.start();
@@ -24,5 +29,11 @@ export class NgProgressInterceptor implements HttpInterceptor {
         this._ngProgress.complete();
       }
     }));
+  }
+
+  private checkUrl(req: HttpRequest<any>) {
+    const url = req.url.toLowerCase();
+    const found = this._config.silentApis.find((u) => url.startsWith(u));
+    return !!found;
   }
 }
